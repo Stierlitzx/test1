6aba18e8e8b07a3d15f98bbaf01922720ec11743
Stierlitzx <141738510+Stierlitzx@users.noreply.github.com>
2023-11-24 12:04:48 +0600

    Add files via upload
    v2

---
+++ nurba.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class nurba {
+    public static void main(String[] args) {
+    	double width = 1;
+    	double heigth = 1;
+    	// System.out.println(getArea(width, heigth));
+    	System.out.println("Hello");
+    	
+    }
+    public static getArea(double a, double b){
+
+    }
+    public static getPerimeter(double a, double b){
+    	
+    }
+}
